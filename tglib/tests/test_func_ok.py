8f80ce32e60702f33f902d2d3692646acdea23be
gekone1 <ivangasparott@gmail.com>
2024-02-19 22:08:10 +0100

    edited tests/test_func_ok.py

--- tglib/tests/test_func_ok.py
+++ tglib/tests/test_func_ok.py
@@ -1,13 +1,22 @@
+import sys
+
+if __name__ != '__main__':
+    import os
+    print(f'{os.path.basename(__file__)} must be launched from __main__')
+    sys.exit(1)
+
+sys.path.append('../..')
 from tglib.update_manager import _func_ok
 
-print('test for checker and func for update_manager,\nthey must take 1 argument and they cannot be generators.')
+print('\n- Testing checker and function of the update_managers, they must take 1 argument and they cannot be generators.\n')
 
-print('Testing checker\n')
-print(f'{_func_ok(lambda x: ...)=}')
-print(f'{_func_ok(lambda *ar, x: ...)=}')
-print(f'{_func_ok(lambda *, x: ...)=}')
-print(f'{_func_ok(lambda x, **kw: ...)=}')
-print(f'{_func_ok(lambda x, /: ...)=}')
+print("~" * 55)
+print('\n- Testing checker:\n')
+print(f'{_func_ok(lambda x: ...) = }')
+print(f'{_func_ok(lambda x, /: ...) = }')
+print(f'{_func_ok(lambda *ar, x: ...) = }')
+print(f'{_func_ok(lambda *, x: ...) = }')
+print(f'{_func_ok(lambda x, **kw: ...) = }\n')
 
 gen_str = '''\
 def gen(x):
@@ -16,11 +25,13 @@ def gen(x):
 def gen(x):
     yield
 
-print('Testing generator:', gen_str, sep='\n')
-print(f'{_func_ok(gen)=}')
+print("~" * 55)
+print('\n- Checking for a normal function using a generator:', gen_str, sep='\n\n')
+print(f'\n{_func_ok(gen) = }\n')
 
-print('Testing async function\n')
-print(f'{_func_ok(lambda x: True, must_be_coro=True)=}')
+print("~" * 55)
+print('\n- Checking for an async function using a lambda function:\n')
+print(f'{_func_ok(lambda x: True, must_be_coro = True) = }\n')
 
 async_func_str = '''\
 async def foo(x):
@@ -29,8 +40,10 @@ async def foo(x):
 async def foo(x):
     ...
 
-print('async func is:', async_func_str, sep='\n')
-print(f'{_func_ok(foo, must_be_coro=True)=}')
+print("~" * 55)
+print('\n- Checking for an async function using an async function:', async_func_str, sep='\n\n')
+print(f'\n{_func_ok(foo, must_be_coro = True) = }\n')
+print("~" * 55)
 
 async_gen_str = '''\
 async def bar(x):
@@ -39,6 +52,7 @@ async def bar(x):
 async def bar(x):
     yield
 
-print('async gen is:', async_gen_str, sep='\n')
-print(f'{_func_ok(bar, must_be_coro=True)=}')
+print('\nChecking for an async function using an async generator:', async_gen_str, sep='\n\n')
+print(f'\n{_func_ok(bar, must_be_coro = True) = }\n')
 
+print("~" * 55)
