a82c96d17100c602be9075b5a3c413b902a78f97
gekone1 <ivangasparott@gmail.com>
2024-02-24 11:53:25 +0100

    update answer_inline_query.__doc__

--- tglib/client.py
+++ tglib/client.py
@@ -2847,8 +2847,23 @@ class Client(TelegramApi):
     ) -> Literal[True]:
         '''
         https://core.telegram.org/bots/api#answerinlinequery
+
         Use this method to send answers to an inline query. On success,
         True is returned. No more than 50 results per query are allowed.
+
+        :param inline_query_id: Unique identifier for the answered query.
+        :type inline_query_id: :obj:`str`
+        :param results: A JSON-serialized array of results for the inline query.
+        :type results: :obj:`list` of :obj:`~tglib.types.InlineQueryResult`
+        :param cache_time: The maximum amount of time in seconds that the result of the inline query may be cached on the server. Defaults to :obj:`300`.
+        :type cache_time: :obj:`int` or :obj:`None`
+        :param is_personal: Pass :obj:`True` if results may be cached on the server side only for the user that sent the query. By default, results may be returned to any user who sends the same query.
+        :type is_personal: :obj:`bool` or :obj:`None`
+        :param next_offset: Pass the offset that a client should send in the next query with the same text to receive more results. Pass an empty string if there are no more results or if you don't support pagination. Offset length can't exceed 64 bytes.
+        :type next_offset: :obj:`str` or :obj:`None`
+        :param button: A JSON-serialized object describing a button to be shown above inline query results.
+        :type button: :obj:`~tglib.types.InlineQueryResultsButton` or :obj:`None`
+        :rtype: :obj:`True`
         '''
         params = {
             'inline_query_id': inline_query_id,
