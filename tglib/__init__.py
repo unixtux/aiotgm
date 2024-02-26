6c38b27780e0b8d36209554ddcb7bfc94ac76351
gekone1 <ivangasparott@gmail.com>
2024-02-26 14:33:46 +0100

    1.3.0

--- tglib/__init__.py
+++ tglib/__init__.py
@@ -1,15 +1,7 @@
 #!/bin/env python3
 
-__version__ = '1.2.9'
+__version__ = '1.3.0'
 VERSION = __version__
-
-__all__ = (
-    '__version__',
-    'VERSION',
-    'Client',
-    'NextFunction',
-    'TelegramError',
-)
 
 from .client import (
     Client,
