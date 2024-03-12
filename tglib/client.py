dcc809b6e67f8812a0c2a4944b75d9d07f93f0e0
gekone1 <ivangasparott@gmail.com>
2024-03-12 02:18:04 +0100

    update message_reaction_manager.__doc__

--- tglib/client.py
+++ tglib/client.py
@@ -188,8 +188,12 @@ class Client(TelegramApi):
     @property
     def message_reaction_manager(self) -> UpdateManager:
         '''
-        .. automethod:: tglib.update_manager.UpdateManager::add_rule
-            :no-index:
+        .. method:: add_rule(checker, coroutine, /)
+
+            :param checker: A function that takes only one argument to filter an incoming :obj:`~tglib.types.MessageReactionUpdated` :obj:`~tglib.types.Update`.
+            :type checker: :obj:`Callable[[MessageReactionUpdated], Any]`
+            :param coroutine: A `coroutine <https://docs.python.org/3/library/asyncio-task.html#coroutines>`_ that will be `awaited <https://docs.python.org/3/library/asyncio-task.html#awaitables>`_ if the :obj:`~tglib.types.Message` passes the check. It must takes only one argument, it will be processed as :obj:`~tglib.types.MessageReactionUpdated`.
+            :type coroutine: :obj:`Callable[[MessageReactionUpdated], Awaitable]`
         '''
         return self._message_reaction_manager
 
