864059b8c5877e62d3daf6812f140f4e2ad77feb
gekone1 <ivangasparott@gmail.com>
2024-03-13 20:11:20 +0100

    update utils.escape_markdown()

--- aiotele/utils.py
+++ aiotele/utils.py
@@ -22,4 +22,9 @@ MARKDOWN_ESCAPES = {
 }
 
 def escape_markdown(text: str) -> str:
+    if not isinstance(text, str):
+        raise TypeError(
+            'In escape_markdown(), text must'
+            f' be str, got {text.__class__.__name__}'
+        )
     return text.translate(str.maketrans(MARKDOWN_ESCAPES))
