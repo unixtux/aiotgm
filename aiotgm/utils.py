d68bea44170db42e26794dc21597936f0837f627
gekone1 <ivangasparott@gmail.com>
2024-03-17 17:49:39 +0100

    update utils.py

--- aiotgm/utils.py
+++ aiotgm/utils.py
@@ -26,15 +26,10 @@ MARKDOWN_ESCAPES = {
     '!': '\!'
 }
 
-def escape_markdown(text: str) -> str:
-    if not isinstance(text, str):
-        raise TypeError(
-            'In escape_markdown(), text must'
-            f' be str, got {text.__class__.__name__}'
-        )
-    return text.translate(str.maketrans(MARKDOWN_ESCAPES))
+def escape_markdown(text) -> str:
+    return str(text).translate(str.maketrans(MARKDOWN_ESCAPES))
 
 def seconds_until_next_hour() -> float:
-    from datetime import datetime as dtm
-    now = dtm.now()
+    from datetime import datetime
+    now = datetime.now()
     return 3600 - (now.minute * 60 + now.second)
