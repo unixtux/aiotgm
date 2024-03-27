7a40e8c1001bc580f30041596d4e5d84c1a344eb
gekone1 <ivangasparott@gmail.com>
2024-03-27 12:26:40 +0100

    Update unpin_all_forum_topic_messages() docs

--- aiotgm/client.py
+++ aiotgm/client.py
@@ -4864,7 +4864,13 @@ class Client(TelegramApi):
 
         Use this method to clear the list of pinned messages in a forum topic.
         The bot must be an administrator in the chat for this to work and must have the
-        can_pin_messages administrator right in the supergroup. Returns True on success.
+        *can_pin_messages* administrator right in the supergroup. Returns :obj:`True` on success.
+
+        :param chat_id: Unique identifier for the target chat or username of the target supergroup (in the format ``@supergroupusername``).
+        :type chat_id: :obj:`int` or :obj:`str`
+        :param message_thread_id: Unique identifier for the target message thread of the forum topic.
+        :type message_thread_id: :obj:`int`
+        :rtype: :obj:`True`
         '''
         params = {
             'chat_id': chat_id,
