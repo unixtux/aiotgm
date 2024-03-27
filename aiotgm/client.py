19dff1c4f5839aaeb2f6fc228691ca35d6e412e5
gekone1 <ivangasparott@gmail.com>
2024-03-27 12:35:08 +0100

    Update unpin_chat_message() docs

--- aiotgm/client.py
+++ aiotgm/client.py
@@ -4907,9 +4907,17 @@ class Client(TelegramApi):
     ) -> Literal[True]:
         '''
         https://core.telegram.org/bots/api#unpinchatmessage
-        Use this method to remove a message from the list of pinned messages in a chat. If the chat is not a
-        private chat, the bot must be an administrator in the chat for this to work and must have the 'can_pin_messages'
-        administrator right in a supergroup or 'can_edit_messages' administrator right in a channel. Returns True on success.
+
+        Use this method to remove a message from the list of pinned messages in a chat.
+        If the chat is not a private chat, the bot must be an administrator in the chat for this
+        to work and must have the 'can_pin_messages' administrator right in a supergroup or
+        'can_edit_messages' administrator right in a channel. Returns :obj:`True` on success.
+
+        :param chat_id: Unique identifier for the target chat or username of the target channel (in the format ``@channelusername``).
+        :type chat_id: :obj:`int` or :obj:`str`
+        :param message_id: Identifier of a message to unpin. If not specified, the most recent pinned message (by sending date) will be unpinned.
+        :type message_id: :obj:`int`, optional
+        :rtype: :obj:`True`
         '''
         params = {
             'chat_id': chat_id
