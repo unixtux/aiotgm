42d412ac87aa28b2938fdf437652e4e28f908ed3
gekone1 <ivangasparott@gmail.com>
2024-03-29 07:26:31 +0100

    Update tests

--- tests/parse_methods.py
+++ tests/parse_methods.py
@@ -1,6 +1,21 @@
 #!/bin/env python3
 
 if __name__ == '__main__':
+    import logging
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
+
+    while True:
+        logger_level = input('Do you want to use logger.level INFO? (Y) or DEBUG (n) ')
+
+        if logger_level.lower() in ('y', str()):
+            logger.setLevel(20)
+            break
+        elif logger_level.lower() == 'n':
+            logger.setLevel(10)
+            break
+
     import re
 
     with open('../aiotgm/client.py') as r:
@@ -14,24 +29,24 @@ if __name__ == '__main__':
     for line in lines:
         match_is_none = is_not_none.search(line)
         if match_is_none:
-            print(line)
+            logger.debug(line)
             group = match_is_none.group(1, 2, 3)
             if not (group[0].replace('self.', '') == group[1] == group[2].replace('self.', '')):
-                print('is wrong')
+                logger.debug('is wrong')
                 errors.append((group[0], group[1], group[2]))
             else:
-                print('is correct')
+                logger.debug('is correct')
                 corrects.append((group[0], group[1], group[2]))
         match_is_params = is_params.match(line)
         if match_is_params:
-            print(line)
+            logger.debug(line)
             group = match_is_params.group(1, 2)
             if not (group[0]) == group[1]:
-                print('is wrong')
+                logger.debug('is wrong')
                 errors.append((group[0], group[1]))
             else:
-                print('is correct')
+                logger.debug('is correct')
                 corrects.append((group[0], group[1]))
 
-    print('len() of errors are:', len(errors))
-    print('len() of correct is:', len(corrects))
+    logger.info('len() of errors are: %s' % len(errors))
+    logger.info('len() of correct is: %s' % len(corrects))
