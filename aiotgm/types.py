c5f23b56a82ca512c9106a4d186d9543ac6fcb59
gekone1 <ivangasparott@gmail.com>
2024-03-29 21:41:07 +0100

    Update types. replies, docs

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -2409,6 +2409,41 @@ class File(TelegramType):
         self.file_path = file_path
 
 
+class ForceReply(TelegramType):
+    '''
+    https://core.telegram.org/bots/api#forcereply
+
+    Upon receiving a message with this object, Telegram clients will display a reply
+    interface to the user (act as if the user has selected the bot's message and tapped 'Reply').
+    This can be extremely useful if you want to create user-friendly step-by-step interfaces without
+    having to sacrifice `privacy mode <https://core.telegram.org/bots/features#privacy-mode>`_.
+
+        **Example**: A `poll bot <https://t.me/PollBot>`_ for groups runs in privacy mode
+        (only receives commands, replies to its messages and mentions).
+        There could be two ways to create a new poll:
+
+            - Explain the user how to send a command with parameters (e.g. /newpoll question answer1 answer2). May be appealing for hardcore users but lacks modern day polish.
+            - Guide the user through a step-by-step process. 'Please send me your question', 'Cool, now let's add the first answer option', 'Great. Keep adding answer options, then send /done when you're ready'.
+
+        The last option is definitely more attractive. And if you
+        use :obj:`~aiotgm.types.ForceReply` in your bot's questions,
+        it will receive the user's answers even if it only receives
+        replies, commands and mentions - without any extra work for the user.
+
+    :param input_field_placeholder: The placeholder to be shown in the input field when the reply is active; 1-64 characters.
+    :type input_field_placeholder: :obj:`str`, optional
+    :param selective: Use this parameter if you want to force reply from specific users only. Targets: 1) users that are @mentioned in the *text* of the :obj:`~aiotgm.types.Message` object; 2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message.
+    :type selective: :obj:`bool`, optional
+    '''
+    def __init__(
+        self,
+        input_field_placeholder: Optional[str] = None,
+        selective: Optional[bool] = None
+    ):
+        self.force_reply: Literal[True] = True
+        self.input_field_placeholder = input_field_placeholder
+        self.selective = selective
+
 
 
 
@@ -4198,24 +4233,6 @@ class InlineKeyboardMarkup(TelegramType):
 
         self.inline_keyboard = keyboard
 
-
-class ForceReply(TelegramType):
-    '''
-    https://core.telegram.org/bots/api#forcereply
-
-    Upon receiving a message with this object, Telegram clients will display a reply
-    interface to the user (act as if the user has selected the bot's message and tapped 'Reply').\n
-    This can be extremely useful if you want to create user-friendly step-by-step interfaces without having to sacrifice privacy mode.
-    '''
-    def __init__(
-        self,
-        force_reply: Literal[True] = True,
-        input_field_placeholder: Optional[str] = None,
-        selective: Optional[bool] = None
-    ):
-        self.force_reply = force_reply
-        self.input_field_placeholder = input_field_placeholder
-        self.selective = selective
 
 REPLY_MARKUP_TYPES = Union[InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply]
 '''
