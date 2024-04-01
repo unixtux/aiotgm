ca478111932730c5fac1431c58f0d9424fc4f09b
gekone1 <ivangasparott@gmail.com>
2024-04-01 11:10:26 +0200

    Update types.InlineQueryResultCachedVoice docs

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -3397,6 +3397,53 @@ class InlineQueryResultCachedVideo(TelegramType):
         self.input_message_content = input_message_content
 
 
+class InlineQueryResultCachedVoice(TelegramType):
+    '''
+    https://core.telegram.org/bots/api#inlinequeryresultcachedvoice
+
+    Represents a link to a voice message stored on the Telegram servers. By default,
+    this voice message will be sent by the user. Alternatively, you can use *input_message_content*
+    to send a message with the specified content instead of the voice message.
+
+    :param id: Unique identifier for this result, 1-64 bytes.
+    :type id: :obj:`str`
+    :param voice_file_id: A valid file identifier for the voice message.
+    :type voice_file_id: :obj:`str`
+    :param title: Voice message title.
+    :type title: :obj:`str`
+    :param caption: Caption, 0-1024 characters after entities parsing.
+    :type caption: :obj:`str`, optional
+    :param parse_mode: Mode for parsing entities in the voice message caption. See `formatting options <https://core.telegram.org/bots/api#formatting-options>`_ for more details.
+    :type parse_mode: :obj:`str`, optional
+    :param caption_entities: List of special entities that appear in the caption, which can be specified instead of *parse_mode*.
+    :type caption_entities: :obj:`list` of :obj:`~aiotgm.types.MessageEntity`, optional
+    :param reply_markup: `Inline keyboard <https://core.telegram.org/bots/features#inline-keyboards>`_ attached to the message.
+    :type reply_markup: :obj:`~aiotgm.types.InlineKeyboardMarkup`, optional
+    :param input_message_content: Content of the message to be sent instead of the voice message.
+    :type input_message_content: :obj:`~aiotgm.types.InputMessageContent`, optional
+    '''
+    def __init__(
+        self,
+        id: str,
+        voice_file_id: str,
+        title: str,
+        caption: Optional[str] = None,
+        parse_mode: Optional[str] = None,
+        caption_entities: Optional[list[MessageEntity]] = None,
+        reply_markup: Optional[InlineKeyboardMarkup] = None,
+        input_message_content: Optional[InputMessageContent] = None
+    ):
+        self.type = DEFAULT_INLINE_QUERY_RESULT_CACHED_VOICE
+        self.id = id
+        self.voice_file_id = voice_file_id
+        self.title = title
+        self.caption = caption
+        self.parse_mode = parse_mode
+        self.caption_entities = caption_entities
+        self.reply_markup = reply_markup
+        self.input_message_content = input_message_content
+
+
 
 
 
@@ -5905,36 +5952,6 @@ class InlineQueryResultGame(TelegramType):
         self.id = id
         self.game_short_name = game_short_name
         self.reply_markup = reply_markup
-
-
-class InlineQueryResultCachedVoice(TelegramType):
-    '''
-    https://core.telegram.org/bots/api#inlinequeryresultcachedvoice
-
-    Represents a link to a voice message stored on the Telegram servers.\n
-    By default, this voice message will be sent by the user.\n
-    Alternatively, you can use input_message_content to send a message with the specified content instead of the voice message.
-    '''
-    def __init__(
-        self,
-        id: str,
-        voice_file_id: str,
-        title: str,
-        caption: Optional[str] = None,
-        parse_mode: Optional[str] = None,
-        caption_entities: Optional[list[MessageEntity]] = None,
-        reply_markup: Optional[InlineKeyboardMarkup] = None,
-        input_message_content: Optional[InputMessageContent] = None
-    ):
-        self.type = DEFAULT_INLINE_QUERY_RESULT_CACHED_VOICE
-        self.id = id
-        self.voice_file_id = voice_file_id
-        self.title = title
-        self.caption = caption
-        self.parse_mode = parse_mode
-        self.caption_entities = caption_entities
-        self.reply_markup = reply_markup
-        self.input_message_content = input_message_content
 
 
 class SentWebAppMessage(TelegramType):
