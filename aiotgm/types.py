92e101b7a907048609d6d3eb535193736c4bd4af
gekone1 <ivangasparott@gmail.com>
2024-04-04 12:42:26 +0200

    Update InputMediaDocument docs

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -4636,6 +4636,19 @@ class InputMediaDocument(TelegramType):
     https://core.telegram.org/bots/api#inputmediadocument
 
     Represents a general file to be sent.
+
+    :param media: File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name. `More information on Sending Files » <https://core.telegram.org/bots/api#sending-files>`_.
+    :type media: :obj:`str`
+    :param thumbnail: Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>. `More information on Sending Files » <https://core.telegram.org/bots/api#sending-files>`_.
+    :type thumbnail: :obj:`~aiotgm.types.InputFile` or :obj:`str`, optional
+    :param caption: Caption of the document to be sent, 0-1024 characters after entities parsing.
+    :type caption: :obj:`str`, optional
+    :param parse_mode: Mode for parsing entities in the document caption. See `formatting options <https://core.telegram.org/bots/api#formatting-options>`_ for more details.
+    :type parse_mode: :obj:`str`, optional
+    :param caption_entities: List of special entities that appear in the caption, which can be specified instead of *parse_mode*.
+    :type caption_entities: :obj:`list` of :obj:`~aiotgm.types.MessageEntity`, optional
+    :param disable_content_type_detection: Disables automatic server-side content type detection for files uploaded using multipart/form-data. Always :obj:`True`, if the document is sent as part of an album.
+    :type disable_content_type_detection: :obj:`bool`, optional
     '''
     def __init__(
         self,
