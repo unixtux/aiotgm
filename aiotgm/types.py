a6d5261a8835517df482865ce1bb9eff8aaefabe
gekone1 <ivangasparott@gmail.com>
2024-04-04 08:09:38 +0200

    Added the fields request_name, request_username, and request_photo to the class KeyboardButtonRequestUsers.

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -5457,12 +5457,18 @@ class KeyboardButtonRequestUsers(TelegramType):
         request_id: int,
         user_is_bot: Optional[bool] = None,
         user_is_premium: Optional[bool] = None,
-        max_quantity: Optional[int] = None
+        max_quantity: Optional[int] = None,
+        request_name: Optional[bool] = None,
+        request_username: Optional[bool] = None,
+        request_photo: Optional[bool] = None
     ):
         self.request_id = request_id
         self.user_is_bot = user_is_bot
         self.user_is_premium = user_is_premium
         self.max_quantity = max_quantity
+        self.request_name = request_name
+        self.request_username = request_username
+        self.request_photo = request_photo
 
 
 class KeyboardButtonRequestChat(TelegramType):
