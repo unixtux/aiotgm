d29ffda33934a9d2e280fe4271977f48c35d18d4
gekone1 <ivangasparott@gmail.com>
2024-04-04 12:14:45 +0200

    Update InputLocationMessageContent docs

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -4500,6 +4500,43 @@ class InputInvoiceMessageContent(TelegramType):
         self.is_flexible = is_flexible
 
 
+class InputLocationMessageContent(TelegramType):
+    '''
+    https://core.telegram.org/bots/api#inputlocationmessagecontent
+
+    Represents the :obj:`content <aiotgm.types.InputMessageContent>`
+    of a location message to be sent as the result of an inline query.
+
+    :param latitude: Latitude of the location in degrees.
+    :type latitude: :obj:`float`
+    :param longitude: Longitude of the location in degrees.
+    :type longitude: :obj:`float`
+    :param horizontal_accuracy: The radius of uncertainty for the location, measured in meters; 0-1500.
+    :type horizontal_accuracy: :obj:`float`, optional
+    :param live_period: Period in seconds for which the location can be updated, should be between 60 and 86400.
+    :type live_period: :obj:`int`, optional
+    :param heading: For live locations, a direction in which the user is moving, in degrees. Must be between 1 and 360 if specified.
+    :type heading: :obj:`int`, optional
+    :param proximity_alert_radius: For live locations, a maximum distance for proximity alerts about approaching another chat member, in meters. Must be between 1 and 100000 if specified.
+    :type proximity_alert_radius: :obj:`int`, optional
+    '''
+    def __init__(
+        self,
+        latitude: float,
+        longitude: float,
+        horizontal_accuracy: Optional[float] = None,
+        live_period: Optional[int] = None,
+        heading: Optional[int] = None,
+        proximity_alert_radius: Optional[int] = None
+    ):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.horizontal_accuracy = horizontal_accuracy
+        self.live_period = live_period
+        self.heading = heading
+        self.proximity_alert_radius = proximity_alert_radius
+
+
 
 
 
@@ -6514,29 +6551,6 @@ class InputTextMessageContent(TelegramType):
         self.parse_mode = parse_mode
         self.entities = entities
         self.link_preview_options = link_preview_options
-
-
-class InputLocationMessageContent(TelegramType):
-    '''
-    https://core.telegram.org/bots/api#inputlocationmessagecontent
-
-    Represents the content of a location message to be sent as the result of an inline query.
-    '''
-    def __init__(
-        self,
-        latitude: float,
-        longitude: float,
-        horizontal_accuracy: Optional[float] = None,
-        live_period: Optional[int] = None,
-        heading: Optional[int] = None,
-        proximity_alert_radius: Optional[int] = None
-    ):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.horizontal_accuracy = horizontal_accuracy
-        self.live_period = live_period
-        self.heading = heading
-        self.proximity_alert_radius = proximity_alert_radius
 
 
 class InputVenueMessageContent(TelegramType):
