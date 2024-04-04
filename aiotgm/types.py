332deac666fd8a58574837a8234588956c0f20bf
gekone1 <ivangasparott@gmail.com>
2024-04-04 12:48:00 +0200

    Update InputMediaPhoto docs

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -4673,6 +4673,17 @@ class InputMediaPhoto(TelegramType):
     https://core.telegram.org/bots/api#inputmediaphoto
 
     Represents a photo to be sent.
+
+    :param media: File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name. `More information on Sending Files » <https://core.telegram.org/bots/api#sending-files>`_.
+    :type media: :obj:`str`
+    :param caption: Caption of the photo to be sent, 0-1024 characters after entities parsing.
+    :type caption: :obj:`str`, optional
+    :param parse_mode: Mode for parsing entities in the photo caption. See `formatting options <https://core.telegram.org/bots/api#formatting-options>`_ for more details.
+    :type parse_mode: :obj:`str`, optional
+    :param caption_entities: List of special entities that appear in the caption, which can be specified instead of *parse_mode*.
+    :type caption_entities: :obj:`list` of :obj:`~aiotgm.types.MessageEntity`, optional
+    :param has_spoiler: Pass :obj:`True` if the photo needs to be covered with a spoiler animation.
+    :type has_spoiler: :obj:`bool`, optional
     '''
     def __init__(
         self,
