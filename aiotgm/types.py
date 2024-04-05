da96f68fd349efd318acd2dd2b3da46259aec8c2
gekone1 <ivangasparott@gmail.com>
2024-04-05 11:08:35 +0200

    Update LoginUrl docs

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -5179,6 +5179,53 @@ class Location(TelegramType):
         self.proximity_alert_radius = proximity_alert_radius
 
 
+class LoginUrl(TelegramType):
+    '''
+    https://core.telegram.org/bots/api#loginurl
+
+    This object represents a parameter of the inline keyboard button used to automatically authorize
+    a user. Serves as a great replacement for the `Telegram Login Widget <https://core.telegram.org/widgets/login>`_
+    when the user is coming from Telegram. All the user needs to do is tap/click a button and confirm that they want to log in.
+
+    Telegram apps support these buttons as of `version 5.7 <https://telegram.org/blog/privacy-discussions-web-bots#meet-seamless-web-bots>`_.
+
+        Sample bot: `@discussbot <https://t.me/discussbot>`_
+
+    :param url:
+        An HTTPS URL to be opened with user authorization data added to the query string when the button is pressed. If the user refuses to provide authorization data, the original URL without information about the user will be opened. The data added is the same as described in `Receiving authorization data <https://core.telegram.org/widgets/login#receiving-authorization-data>`_.
+
+        **NOTE**: You **must** always check the hash of the received data to verify the authentication and the integrity of the data as described in `Checking authorization <https://core.telegram.org/widgets/login#checking-authorization>`_.
+    :type url: :obj:`str`
+    :param forward_text: New text of the button in forwarded messages.
+    :type forward_text: :obj:`str`, optional
+    :param bot_username: Username of a bot, which will be used for user authorization. See `Setting up a bot <https://core.telegram.org/widgets/login#setting-up-a-bot>`_ for more details. If not specified, the current bot's username will be assumed. The url's domain must be the same as the domain linked with the bot. See `Linking your domain to the bot <https://core.telegram.org/widgets/login#linking-your-domain-to-the-bot>`_ for more details.
+    :type bot_username: :obj:`str`, optional
+    :param request_write_access: Pass :obj:`True` to request the permission for your bot to send messages to the user.
+    :type request_write_access: :obj:`bool`, optional
+    '''
+    @classmethod
+    @_parse_result
+    def _dese(cls, res: dict):
+        obj = {}
+        obj['url'] = res.get('url')
+        obj['forward_text'] = res.get('forward_text')
+        obj['bot_username'] = res.get('bot_username')
+        obj['request_write_access'] = res.get('request_write_access')
+        return cls(**obj)
+
+    def __init__(
+        self,
+        url: str,
+        forward_text: Optional[str] = None,
+        bot_username: Optional[str] = None,
+        request_write_access: Optional[bool] = None
+    ):
+        self.url = url
+        self.forward_text = forward_text
+        self.bot_username = bot_username
+        self.request_write_access = request_write_access
+
+
 
 
 
@@ -5232,37 +5279,6 @@ class SwitchInlineQueryChosenChat(TelegramType):
         self.allow_bot_chats = allow_bot_chats
         self.allow_group_chats = allow_group_chats
         self.allow_channel_chats = allow_channel_chats
-
-
-class LoginUrl(TelegramType):
-    '''
-    https://core.telegram.org/bots/api#loginurl
-
-    This object represents a parameter of the inline keyboard button used to automatically authorize
-    a user. Serves as a great replacement for the Telegram Login Widget when the user is coming from
-    Telegram. All the user needs to do is tap/click a button and confirm that they want to log in.
-    '''
-    @classmethod
-    @_parse_result
-    def _dese(cls, res: dict):
-        obj = {}
-        obj['url'] = res.get('url')
-        obj['forward_text'] = res.get('forward_text')
-        obj['bot_username'] = res.get('bot_username')
-        obj['request_write_access'] = res.get('request_write_access')
-        return cls(**obj)
-
-    def __init__(
-        self,
-        url: str,
-        forward_text: Optional[str] = None,
-        bot_username: Optional[str] = None,
-        request_write_access: Optional[bool] = None
-    ):
-        self.url = url
-        self.forward_text = forward_text
-        self.bot_username = bot_username
-        self.request_write_access = request_write_access
 
 
 class User(TelegramType):
