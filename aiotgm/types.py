46f5f27f050f8e339acfe0b558deaebb6b10c458
gekone1 <ivangasparott@gmail.com>
2024-04-06 19:26:56 +0200

    Up ReplyKeyboardMarkup docs

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -3110,7 +3110,7 @@ class InlineKeyboardMarkup(TelegramType):
     This object represents an `inline keyboard <https://core.telegram.org/bots/features#inline-keyboards>`_ that appears right next to the message it belongs to.
 
     :param inline_keyboard: Array of button rows, each represented by an Array of :obj:`~aiotgm.types.InlineKeyboardButton` objects.
-    :type inline_keyboard: :obj:`list` of :obj:`list` of :obj:`~aiotgm.types.InlineKeyboardButton`
+    :type inline_keyboard: :obj:`list` of :obj:`list` of :obj:`~aiotgm.types.InlineKeyboardButton`, optional
     '''
     @classmethod
     @_parse_result
@@ -6764,6 +6764,118 @@ class ReactionTypeEmoji(TelegramType):
         self.emoji = emoji
 
 
+class ReplyKeyboardMarkup(TelegramType):
+    '''
+    https://core.telegram.org/bots/api#replykeyboardmarkup
+
+    This object represents a `custom keyboard <https://core.telegram.org/bots/features#keyboards>`_ with reply
+    options (see `Introduction to bots <https://core.telegram.org/bots/features#keyboards>`_ for details and examples).
+
+    :param keyboard: Array of button rows, each represented by an Array of :obj:`~aiotgm.types.KeyboardButton` objects.
+    :type keyboard: :obj:`list` of :obj:`list` of :obj:`~aiotgm.types.KeyboardButton`, optional
+    :param is_persistent: Requests clients to always show the keyboard when the regular keyboard is hidden. Defaults to :obj:`False`, in which case the custom keyboard can be hidden and opened with a keyboard icon.
+    :type is_persistent: :obj:`bool`, optional
+    :param resize_keyboard: Requests clients to resize the keyboard vertically for optimal fit (e.g., make the keyboard smaller if there are just two rows of buttons). Defaults to :obj:`False`, in which case the custom keyboard is always of the same height as the app's standard keyboard.
+    :type resize_keyboard: :obj:`bool`, optional
+    :param one_time_keyboard: Requests clients to hide the keyboard as soon as it's been used. The keyboard will still be available, but clients will automatically display the usual letter-keyboard in the chat - the user can press a special button in the input field to see the custom keyboard again. Defaults to :obj:`False`.
+    :type one_time_keyboard: :obj:`bool`, optional
+    :param input_field_placeholder: The placeholder to be shown in the input field when the keyboard is active; 1-64 characters.
+    :type input_field_placeholder: :obj:`str`, optional
+    :param selective:
+        Use this parameter if you want to show the keyboard to specific users only. Targets: 1) users that are @mentioned in the text of the :obj:`~aiotgm.types.Message` object; 2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message.
+
+        *Example*: A user requests to change the bot's language, bot replies to the request with a keyboard to select the new language. Other users in the group don't see the keyboard.
+    :type selective: :obj:`bool`, optional
+    '''
+    def __init__(
+        self,
+        keyboard: Optional[list[list[KeyboardButton]]] = None,
+        is_persistent: Optional[bool] = None,
+        resize_keyboard: Optional[bool] = None,
+        one_time_keyboard: Optional[bool] = None,
+        input_field_placeholder: Optional[str] = None,
+        selective: Optional[bool] = None
+    ):
+        self.keyboard = keyboard or []
+        self.is_persistent = is_persistent
+        self.resize_keyboard = resize_keyboard
+        self.one_time_keyboard = one_time_keyboard
+        self.input_field_placeholder = input_field_placeholder
+        self.selective = selective
+
+    def add(self, *buttons: Union[KeyboardButton, str]) -> Self:
+        '''
+        Usage:
+
+        .. code-block:: python3
+
+            markup = ReplyKeyboardMarkup()
+            markup.add(
+                KeyboardButton('x'),
+                KeyboardButton('y'),
+                KeyboardButton('z')
+            )
+            # All the buttons added with this method will be in
+            # the same row, you can change the row width after the
+            # object initialization using the property setter 'row_width'.
+
+        :param buttons: :obj:`KeyboardButtons <aiotgm.types.KeyboardButton>` to add to a new row of the *keyboard*. For simple text buttons, you can pass a :obj:`String` instead of a :obj:`~aiotgm.types.KeyboardButton` object to specify the button text.
+        :type buttons: \*\ :obj:`~aiotgm.types.KeyboardButton` or \*\ :obj:`str`
+        :rtype: :obj:`~aiotgm.types.ReplyKeyboardMarkup`
+        '''
+        self.keyboard.append(buttons)
+        return self
+
+    @property
+    def row_width(self) -> int:
+        '''
+        Usage:
+
+        .. code-block:: python3
+
+            markup.row_width = 2
+            # The keyboard will be rearranged with 2 buttons for each row.
+        '''
+        row_width = 0
+        for nested in self.keyboard:
+            if len(nested) > row_width:
+                row_width = len(nested)
+        return row_width
+
+    @row_width.setter
+    def row_width(self, value: int) -> None:
+
+        keyboard = []
+        nested = []
+        for row in self.keyboard:
+            for button in row:
+                nested.append(button)
+                if len(nested) == value:
+                    keyboard.append(nested)
+                    nested = []
+
+        if nested:
+            keyboard.append(nested)
+
+        self.keyboard = keyboard
+
+
+class ReplyKeyboardRemove(TelegramType):
+    '''
+    https://core.telegram.org/bots/api#replykeyboardremove
+
+    Upon receiving a message with this object, Telegram clients will remove the current custom keyboard and display the default letter-keyboard.\n
+    By default, custom keyboards are displayed until a new keyboard is sent by a bot.\n
+    An exception is made for one-time keyboards that are hidden immediately after the user presses a button (see ReplyKeyboardMarkup).
+    '''
+    def __init__(
+        self,
+        selective: Optional[bool] = None
+    ):
+        self.remove_keyboard: Literal[True] = True
+        self.selective = selective
+
+
 
 
 
@@ -7324,102 +7436,6 @@ class WebAppInfo(TelegramType):
         url: str
     ):
         self.url = url
-
-
-class ReplyKeyboardMarkup(TelegramType):
-    '''
-    https://core.telegram.org/bots/api#replykeyboardmarkup
-
-    This object represents a custom keyboard with reply
-    options (see Introduction to bots for details and examples).
-    '''
-    def __init__(
-        self,
-        keyboard: Optional[list[list[KeyboardButton]]] = None,
-        is_persistent: Optional[bool] = None,
-        resize_keyboard: Optional[bool] = None,
-        one_time_keyboard: Optional[bool] = None,
-        input_field_placeholder: Optional[str] = None,
-        selective: Optional[bool] = None
-    ):
-        self.keyboard = keyboard or []
-        self.is_persistent = is_persistent
-        self.resize_keyboard = resize_keyboard
-        self.one_time_keyboard = one_time_keyboard
-        self.input_field_placeholder = input_field_placeholder
-        self.selective = selective
-
-    def add(self, *buttons: Union[KeyboardButton, str]) -> Self:
-        '''
-        Usage:
-
-        .. code-block:: python3
-
-            markup = ReplyKeyboardMarkup()
-            markup.add(
-                KeyboardButton('x'),
-                KeyboardButton('y'),
-                KeyboardButton('z')
-            )
-            # All the buttons added with this method will be in
-            # the same row, you can change the row width after the
-            # object initialization using the property setter 'row_width'.
-
-        :param buttons: :obj:`KeyboardButtons <aiotgm.types.KeyboardButton>` to add to a new row of the *keyboard*. For simple text buttons, you can pass a :obj:`String` instead of a :obj:`~aiotgm.types.KeyboardButton` object to specify the button text.
-        :type buttons: \*\ :obj:`~aiotgm.types.KeyboardButton` or \*\ :obj:`str`
-        :rtype: :obj:`~aiotgm.types.ReplyKeyboardMarkup`
-        '''
-        self.keyboard.append(buttons)
-        return self
-
-    @property
-    def row_width(self) -> int:
-        '''
-        Usage:
-
-        .. code-block:: python3
-
-            markup.row_width = 2
-            # The keyboard will be rearranged with 2 buttons for each row.
-        '''
-        row_width = 0
-        for nested in self.keyboard:
-            if len(nested) > row_width:
-                row_width = len(nested)
-        return row_width
-
-    @row_width.setter
-    def row_width(self, value: int) -> None:
-
-        keyboard = []
-        nested = []
-        for row in self.keyboard:
-            for button in row:
-                nested.append(button)
-                if len(nested) == value:
-                    keyboard.append(nested)
-                    nested = []
-
-        if nested:
-            keyboard.append(nested)
-
-        self.keyboard = keyboard
-
-
-class ReplyKeyboardRemove(TelegramType):
-    '''
-    https://core.telegram.org/bots/api#replykeyboardremove
-
-    Upon receiving a message with this object, Telegram clients will remove the current custom keyboard and display the default letter-keyboard.\n
-    By default, custom keyboards are displayed until a new keyboard is sent by a bot.\n
-    An exception is made for one-time keyboards that are hidden immediately after the user presses a button (see ReplyKeyboardMarkup).
-    '''
-    def __init__(
-        self,
-        selective: Optional[bool] = None
-    ):
-        self.remove_keyboard: Literal[True] = True
-        self.selective = selective
 
 
 REPLY_MARKUP_TYPES = Union[InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, ForceReply]
