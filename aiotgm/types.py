1b88f75b0560f882f6ae17384961ee982842d9f9
gekone1 <ivangasparott@gmail.com>
2024-04-06 19:32:13 +0200

    Update ReplyKeyboardRemove & ReplyKeyboardMarkup docs

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -6782,7 +6782,7 @@ class ReplyKeyboardMarkup(TelegramType):
     :param input_field_placeholder: The placeholder to be shown in the input field when the keyboard is active; 1-64 characters.
     :type input_field_placeholder: :obj:`str`, optional
     :param selective:
-        Use this parameter if you want to show the keyboard to specific users only. Targets: 1) users that are @mentioned in the text of the :obj:`~aiotgm.types.Message` object; 2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message.
+        Use this parameter if you want to show the keyboard to specific users only. Targets: 1) users that are @mentioned in the *text* of the :obj:`~aiotgm.types.Message` object; 2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message.
 
         *Example*: A user requests to change the bot's language, bot replies to the request with a keyboard to select the new language. Other users in the group don't see the keyboard.
     :type selective: :obj:`bool`, optional
@@ -6864,9 +6864,16 @@ class ReplyKeyboardRemove(TelegramType):
     '''
     https://core.telegram.org/bots/api#replykeyboardremove
 
-    Upon receiving a message with this object, Telegram clients will remove the current custom keyboard and display the default letter-keyboard.\n
-    By default, custom keyboards are displayed until a new keyboard is sent by a bot.\n
-    An exception is made for one-time keyboards that are hidden immediately after the user presses a button (see ReplyKeyboardMarkup).
+    Upon receiving a message with this object, Telegram clients will remove the current custom keyboard
+    and display the default letter-keyboard. By default, custom keyboards are displayed until a new
+    keyboard is sent by a bot. An exception is made for one-time keyboards that are hidden immediately
+    after the user presses a button (see :obj:`~aiotgm.types.ReplyKeyboardMarkup`).
+
+    :param selective:
+        Use this parameter if you want to remove the keyboard for specific users only. Targets: 1) users that are @mentioned in the *text* of the :obj:`~aiotgm.types.Message` object; 2) if the bot's message is a reply to a message in the same chat and forum topic, sender of the original message.
+
+        *Example*: A user votes in a poll, bot returns confirmation message in reply to the vote and removes the keyboard for that user, while still showing the keyboard with poll options to users who haven't voted yet.
+    :type selective: :obj:`bool`, optional
     '''
     def __init__(
         self,
