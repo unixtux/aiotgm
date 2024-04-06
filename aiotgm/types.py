30669e7761eba1941490b6b0d41ce121f2f2d1f0
gekone1 <ivangasparott@gmail.com>
2024-04-06 17:34:31 +0200

    Update PassportElementErrorReverseSide docs

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -6243,8 +6243,15 @@ class PassportElementErrorReverseSide(TelegramType):
     '''
     https://core.telegram.org/bots/api#passportelementerrorreverseside
 
-    Represents an issue with the reverse side of a document.\n
+    Represents an issue with the reverse side of a document.
     The error is considered resolved when the file with reverse side of the document changes.
+
+    :param type: The section of the user's Telegram Passport which has the issue, one of “driver_license”, “identity_card”.
+    :type type: :obj:`str`
+    :param file_hash: Base64-encoded hash of the file with the reverse side of the document.
+    :type file_hash: :obj:`str`
+    :param message: Error message.
+    :type message: :obj:`str`
     '''
     def __init__(
         self,
