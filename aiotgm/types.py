296182342cd6b413b6b7659507aa6543ac41c891
gekone1 <ivangasparott@gmail.com>
2024-07-03 10:42:25 +0200

    Added support for launching Web Apps via t.me link in the class MenuButtonWebApp.

--- aiotgm/types.py
+++ aiotgm/types.py
@@ -5947,7 +5947,7 @@ class MenuButtonWebApp(TelegramType):
 
     :param text: Text on the button.
     :type text: :obj:`str`
-    :param web_app: Description of the Web App that will be launched when the user presses the button. The Web App will be able to send an arbitrary message on behalf of the user using the method :meth:`~aiotgm.Client.answer_web_app_query`.
+    :param web_app: Description of the Web App that will be launched when the user presses the button. The Web App will be able to send an arbitrary message on behalf of the user using the method :meth:`~aiotgm.Client.answer_web_app_query`. Alternatively, a ``t.me`` link to a Web App of the bot can be specified in the object instead of the Web App's URL, in which case the Web App will be opened as if the user pressed the link.
     :type web_app: :obj:`~aiotgm.types.WebAppInfo`
     '''
     @classmethod
