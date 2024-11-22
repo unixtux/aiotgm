9c695d3d36450135a19f3b02ea250331b5771970
gekone1 <ivangasparott@gmail.com>
2024-11-22 16:34:29 +0100

    updating

--- aiotgm/client.py
+++ aiotgm/client.py
@@ -4536,6 +4536,7 @@ class Client(TelegramApi):
         supports_streaming: Optional[bool] = None,
         disable_notification: Optional[bool] = None,
         protect_content: Optional[bool] = None,
+        allow_paid_broadcast: Optional[bool] = None,
         message_effect_id: Optional[str] = None,
         reply_parameters: Optional[ReplyParameters] = None,
         reply_markup: Optional[REPLY_MARKUP_TYPES] = None
@@ -4580,6 +4581,8 @@ class Client(TelegramApi):
         :type disable_notification: :obj:`bool`, optional
         :param protect_content: Protects the contents of the sent message from forwarding and saving.
         :type protect_content: :obj:`bool`, optional
+        :param allow_paid_broadcast: Pass :obj:`True` to allow up to 1000 messages per second, ignoring `broadcasting limits <https://core.telegram.org/bots/faq#how-can-i-message-all-of-my-bot-39s-subscribers-at-once>`_ for a fee of 0.1 Telegram Stars per message. The relevant Stars will be withdrawn from the bot's balance.
+        :type allow_paid_broadcast: :obj:`bool`, optional
         :param message_effect_id: Unique identifier of the message effect to be added to the message; for private chats only.
         :type message_effect_id: :obj:`str`, optional
         :param reply_parameters: Description of the message to reply to.
@@ -4608,6 +4611,7 @@ class Client(TelegramApi):
         if disable_notification is not None: params['disable_notification'] = disable_notification
         if protect_content is not None: params['protect_content'] = protect_content
         elif self.protect_content is not None: params['protect_content'] = self.protect_content
+        if allow_paid_broadcast is not None: params['allow_paid_broadcast'] = allow_paid_broadcast
         if message_effect_id is not None: params['message_effect_id'] = message_effect_id
         if reply_parameters is not None: params['reply_parameters'] = reply_parameters
         if reply_markup is not None: params['reply_markup'] = reply_markup
